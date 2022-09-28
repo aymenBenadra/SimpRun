f068d55376791ebacbfad82944eea9542fb26670
Mohammed-Aymen Benadra <aymanbenadra16@gmail.com>
2022-09-28 16:52:45 +0100

    Created MemoryCollectionDriver to handle interactions with the collection of T objects it holds

---
+++ src/IDriver.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public interface IDriver<T> {
+    void add(T o);
+    void delete(String id);
+    Object get(String id);
+    ArrayList<T> getAll();
+    void update(String id, T o);
+    int getCount();
+}
---
+++ src/MemoryCollectionDriver.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+
+public class MemoryCollectionDriver<T extends Objectable> implements IDriver<T> {
+    private final ArrayList<T> entities;
+
+    public MemoryCollectionDriver() {
+        entities = new ArrayList<>();
+    }
+
+    public void add(T e) {
+        entities.add(e);
+    }
+
+    public void delete(String id) {
+        entities.removeIf(e -> {
+            return e.getId().equals(id);
+        });
+    }
+
+    public T get(String id) {
+        for (T e : entities) {
+            if (e.getId().equals(id)) {
+                return e;
+            }
+        }
+        return null;
+    }
+
+    public ArrayList<T> getAll() {
+        return entities;
+    }
+
+    public void update(String id, T e) {
+        for (int i = 0; i < entities.size(); i++) {
+            if (entities.get(i).getId().equals(id)) {
+                entities.set(i, e);
+            }
+        }
+    }
+
+    public int getCount() {
+        return entities.size();
+    }
+}
