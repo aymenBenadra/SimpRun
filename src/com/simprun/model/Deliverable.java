33f500f4aa011a0f3910d131b3f63f10e0aa8f79
Mohammed-Aymen Benadra <aymanbenadra16@gmail.com>
2022-10-02 02:45:50 +0100

    Fixed typo from Delivrable to Deliverable

--- src/com/simprun/model/Deliverable.java
+++ src/com/simprun/model/Deliverable.java
@@ -3,17 +3,17 @@ package com.simprun.model;
 import java.util.Date;
 import java.util.UUID;
 
-public class Delivrable implements IObjectable {
+public class Deliverable implements IObjectable {
     private final String id;
     private final String link;
     private final Date createdAt;
     private final Brief brief;
     private final Apprenant apprenant;
 
-    public Delivrable(String link, Date createdAt, Brief brief, Apprenant apprenant) {
+    public Deliverable(String link, Brief brief, Apprenant apprenant) {
         this.id = UUID.randomUUID().toString();
         this.link = link;
-        this.createdAt = createdAt;
+        this.createdAt = new Date();
         this.brief = brief;
         this.apprenant = apprenant;
     }
@@ -40,7 +40,7 @@ public class Delivrable implements IObjectable {
 
     @Override
     public String toString() {
-        return "Delivrable {" +
+        return "Deliverable {" +
                 "id='" + id + '\'' +
                 ", link='" + link + '\'' +
                 ", createdAt='" + createdAt.toString() + '\'' +
